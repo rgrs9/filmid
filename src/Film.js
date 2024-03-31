9eafe773eb5c4de48ee037a945f55448c5e624cf
rgrs9 <108997831+rgrs9@users.noreply.github.com>
2024-03-31 17:11:29 +0300

    Update Film.js

--- src/Film.js
+++ src/Film.js
@@ -2,7 +2,7 @@ function Film ({title, overview, posterPath}) {
     const pildiUrl = `https://image.tmdb.org/t/p/w500${posterPath}`;
     return (
         <div className="card" style={{width: 400}}>
-            <img src={pildiUrl} className="card-img-top"/>
+            <img src={pildiUrl} className="card-img-top" alt={title}/>
             <div className="card-body">
                 <h2 className="card-title">{title || 'Tundmatu film'}</h2>
                 <p className="card-text">{overview|| 'Sisu pole saadaval'}</p>
